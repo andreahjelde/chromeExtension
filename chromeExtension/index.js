a1fdfd76a7b7a78a6cbf97ff9b983812ba035ae7
Andrea <s364756@oslomet.no>
2023-01-30 19:56:09 +0100

    La til flere knapper samt API for lagring av tab

--- chromeExtension/index.js
+++ chromeExtension/index.js
@@ -3,32 +3,70 @@
 //Hvis mulig, bruk Const. Hvis det ikke er mulig, bruk Let.
 
 let myLeads = []
-const input = document.getElementById("input-el")
+const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 const inputBtn = document.getElementById("input-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const tabBtn = document.getElementById("tab-btn")
 
-inputBtn.addEventListener("click", function () {
-    myLeads.push(input.value)
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalStorage)
 
-    //Kaller på funksjonen som skriver ut innholdet i inputet, og legger verdiene i en liste 
-    renderLeads()
+// Sjekker om leadsFromLocalStorage er sann, hvis sann - sett myLeads til den verdien og kall på funksjonen render()
+if (leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
 
-    //Tømmer input-feltet
-    if (input.value != "") {
-        input.value = ""
-    }
+tabBtn.addEventListener("click", function () {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads))
+        render(myLeads)
+    })
 })
 
+
 //Funksjon som skriver ut innholdet i listen
-function renderLeads() {
+function render(leads) {
     let listItems = ""
 
-    for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+    for (let i = 0; i < leads.length; i++) {
+        //listItems += "<li><a target='_blank' href=' " + leads[i] + "'>" + leads[i] + "</a></li>"
+
+        //Alternativ måte å skrive på
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'> 
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+
     }
 
     ulEl.innerHTML = listItems
 }
+
+// Når Delete blir dobbeltklikket, slett localStore, myLeads og DOM
+deleteBtn.addEventListener("click", function () {
+    console.log("double clikc")
+    myLeads = []
+    render(myLeads)
+})
+
+inputBtn.addEventListener("click", function () {
+    myLeads.push(inputEl.value)
+
+    //Tømmer input-feltet
+    inputEl.value = ""
+
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    console.log(localStorage.getItem("myLeads"))
+    //Kaller på funksjonen som skriver ut innholdet i inputet, og legger verdiene i en liste 
+    render(myLeads)
+})
+
 
 
 
@@ -37,3 +75,6 @@ function renderLeads() {
     //create element: const li = document.createElement("li")
     //Set text content: li.textContent = myLeads[i]
     //appen = sette noe på slutten : ulEl.append(li)
+
+
+
